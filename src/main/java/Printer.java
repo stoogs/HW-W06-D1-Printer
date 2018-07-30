8f9393f432776729f58b7c2c21f018276a8ed108
Stuart Black <stuartblack@gmail.com>
2018-07-30 21:40:32 +0100

    Adopted copies * pages method

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,52 +1,48 @@
 public class Printer {
     private int paperLeft;
     private int tonerVolume;
-    private int manyCopies;
+    private int copiesToMake;
+    private int pagesToPrint;
+    private int jobTotal;
 
     public Printer(int paperLeft, int tonerVolume){
        this.paperLeft = paperLeft;
        this.tonerVolume = tonerVolume;
     }
-
     public int getpaperLeft(){
         return this.paperLeft;
     }
-
     public int getTonerVolume(){
         return this.tonerVolume;
     }
-
     public void setPaperLeft(int paperLeft){
         this.paperLeft = paperLeft;
     }
-
     public void setTonerVolume(int tonerVolume){
         this.tonerVolume = tonerVolume;
     }
-
     public void status(){
         System.out.println("Paper Left " + this.paperLeft);
         System.out.println("Toner Left " + this.tonerVolume);
     }
-
-    public void print() {
+    public void print(int copiesToMake, int pagesToPrint) {
         System.out.println("---- Printing... ----");
-        if (this.paperLeft > 0 && this.tonerVolume > 0) {
-            this.paperLeft -= 1;
-            this.tonerVolume -= 1;
+        this.jobTotal = copiesToMake * pagesToPrint;
+        if (this.paperLeft > this.jobTotal && this.tonerVolume > this.jobTotal) {
+            this.paperLeft -= this.jobTotal;
+            this.tonerVolume -= this.jobTotal;
             this.status();
         } else {
-            System.out.println("Sorry, no paper");
+            System.out.println("Sorry, not enough paper/toner");
             this.status();
         }
     }
-
-    public void refill(){
-        this.paperLeft = 100;
+    public void refill(int num){
+        System.out.println(num + " sheets of paper added");
+        this.paperLeft += num;
         System.out.println("---- Paper refilled ----");
         this.status();
     }
-
     public void empty(){
         this.paperLeft = 0;
         System.out.println("Paper Emptied");
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,13 +2,17 @@
     public class Runner {
         public static void main(String[] args) {
             Printer epson = new Printer(100,100);
+            // Printer's toner and pages available
             epson.status();
-            epson.print();
+            // Print copies of number of pages
+            epson.print(3,5);
+            // empty paper tray
             epson.empty();
-            epson.print();
-            epson.refill();
-            epson.print();
-
+            epson.print(1,2);
+            // refill paper tray with 55 sheets
+            epson.refill(55);
+            // cause print failure for not enough toner/paper
+            epson.print(10,12);
         }
 
 
